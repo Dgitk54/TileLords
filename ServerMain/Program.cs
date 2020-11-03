c74e27e04190d3ed7086f9a0bd238c466f187743
Kat <ainarul123@gmail.com>
2020-11-03 15:07:21 +0100

    Test Push

--- ServerMain/Program.cs
+++ ServerMain/Program.cs
@@ -6,7 +6,7 @@ namespace ServerMain
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World Test!");
         }
     }
 }
