afc5c10da1f22d8f3c3f21f06317df6422fa5bbd
Dk <dvsk76@outlook.com>
2020-11-03 14:03:56 +0100

    Initial Commit

---
+++ ServerMain/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ServerMain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
