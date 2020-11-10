3a5f35b1edcca96a704a6264027d1ddd2b0d413f
Dk <dvsk76@outlook.com>
2020-11-10 16:47:29 +0100

    Refactored project structure, added first common interfaces shared by client and server

---
+++ GameModel/INetworkMessageProvider.cs
@@ -0,0 +1,15 @@
+﻿using DataModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataModel
+{
+    /// <summary>
+    /// Interface which is responsible to provide a stream of messages received from the network.
+    /// </summary>
+    interface NetworkMessageProvider
+    {
+        IObservable<INetworkMessage> NetworkMessageReceived();
+    }
+}
