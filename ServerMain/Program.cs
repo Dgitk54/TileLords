c0df975d959990e7e2e153810459a486ae58aade
Kat <ainarul123@gmail.com>
2020-11-11 13:44:35 +0100

    Created test discard server

--- ServerMain/Program.cs
+++ ServerMain/Program.cs
@@ -1,13 +1,53 @@
-﻿using DataModel;
+﻿
+using DotNetty.Transport.Bootstrapping;
+using DotNetty.Transport.Channels;
+using DotNetty.Transport.Channels.Sockets;
+using Server;
 using System;
+using System.Threading.Tasks;
 
 namespace ServerMain
 {
     class Program
     {
-        static void Main(string[] args)
+
+        private static int port = 8080;
+
+       
+        static async Task RunServerAsync()
         {
 
+            var bossGroup = new MultithreadEventLoopGroup(1);
+            var workerGroup = new MultithreadEventLoopGroup();
+            Console.WriteLine("reached here 1");
+            try
+            {
+                var bootstrap = new ServerBootstrap();
+                bootstrap
+                    .Group(bossGroup, workerGroup)
+                    .Channel<TcpServerSocketChannel>()
+                    .Option(ChannelOption.SoBacklog, 100)
+                    .ChildHandler(new ActionChannelInitializer<ISocketChannel>(channel =>
+                    {
+                        IChannelPipeline pipeline = channel.Pipeline;
+                        pipeline.AddLast(new TestServerHandler());
+                    }));
+
+               
+                IChannel bootstrapChannel = await bootstrap.BindAsync(port);
+
+                Console.ReadLine();
+
+                await bootstrapChannel.CloseAsync();
+                Console.WriteLine("reached here 2");
+            }
+            finally
+            {
+                Task.WaitAll(bossGroup.ShutdownGracefullyAsync(), workerGroup.ShutdownGracefullyAsync());
+            }
         }
+
+        public static void Main() => RunServerAsync().Wait();
     }
 }
+
