eb727509e984f9451c46cdae3dbc96c1ebbbd31a
Kat <ainarul123@gmail.com>
2020-12-15 09:09:13 +0100

    Added back some missing MiniTileTypes

--- DataModel.Common/MiniTileType.cs
+++ DataModel.Common/MiniTileType.cs
@@ -11,12 +11,17 @@ namespace DataModel.Common
         Grass_River2,
         Snow_Tile,
         Snow_River,
+        Snow_River2,
         Snow_PatchyGrass,
         Sand_Tile,
         Sand_River,
         Rock_Tile,
         Rock_River,
+        Rock_River2,
         WaterBody_Tile,
+        Mud_Grass,
+        Mud_Marsh,
+        Mud_Tile,
         Unknown_Tile
 
     }
