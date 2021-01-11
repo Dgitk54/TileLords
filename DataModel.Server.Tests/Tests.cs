3a140d4916cb65a6d76460db56bce019ab0da90d
Dk <dvsk76@outlook.com>
2021-01-11 08:52:06 +0100

    Added test coverage for players standing on same tile.

--- DataModel.Server.Tests/Tests.cs
+++ DataModel.Server.Tests/Tests.cs
@@ -709,7 +709,167 @@ namespace ClientIntegration
 
         }
 
+        [Test]
+        public void PlayersMoveOnSameTileSeeEachOther()
+        {
+            //Setup Players:
+            var p1State = new Subject<bool>();
+            var p1Gps = new Subject<PlusCode>();
+            var p1Bus = new ClientEventBus();
+            var player1 = new ObservablePlayer()
+            {
+                Name = "Player1",
+                ConnectionStatus = p1State,
+                PlayerObservableLocationStream = p1Gps.AsObservable(),
+                ClientBus = p1Bus
+            };
 
+
+            var p2State = new Subject<bool>();
+            var p2Gps = new Subject<PlusCode>();
+            var p2Bus = new ClientEventBus();
+            var player2 = new ObservablePlayer()
+            {
+                Name = "Player2",
+                ConnectionStatus = p2State,
+                PlayerObservableLocationStream = p2Gps.AsObservable(),
+                ClientBus = p2Bus
+            };
+
+
+            var p3State = new Subject<bool>();
+            var p3Gps = new Subject<PlusCode>();
+            var p3Bus = new ClientEventBus();
+            var player3 = new ObservablePlayer()
+            {
+                Name = "Player3",
+                ConnectionStatus = p3State,
+                PlayerObservableLocationStream = p3Gps.AsObservable(),
+                ClientBus = p3Bus
+            };
+
+
+
+            //Setup Bus & Handlers
+            var cleanUp = new List<IDisposable>();
+            var eventBus = new ServerEventBus();
+
+            var movementHandler = new PlayerMovementTileUpdater(eventBus);
+            var tileConentHandler = new PlayerTileContentHandler(eventBus);
+            var playersOnlineHandler = new PlayersOnlineHandler(eventBus);
+
+            cleanUp.Add(movementHandler.AttachToBus());
+            cleanUp.Add(movementHandler.AttachCleanup());
+            cleanUp.Add(tileConentHandler.AttachToBus());
+            cleanUp.Add(playersOnlineHandler.AttachToBus());
+
+
+
+            var p1Login = new PlayerLoggedInEvent() { Player = player1 };
+            var p2Login = new PlayerLoggedInEvent() { Player = player2 };
+            var p3Login = new PlayerLoggedInEvent() { Player = player3 };
+
+
+
+            var mapEvents = new List<ServerMapEvent>();
+            var p1ContentEvents = new List<ServerTileContentEvent>();
+            var p2ContentEvents = new List<ServerTileContentEvent>();
+            eventBus.GetEventStream<ServerMapEvent>().Subscribe(v =>
+            {
+                mapEvents.Add(v);
+
+            });
+
+            p1Bus.GetEventStream<ServerTileContentEvent>().Subscribe(v =>
+            {
+                p1ContentEvents.Add(v);
+            });
+
+            p2Bus.GetEventStream<ServerTileContentEvent>().Subscribe(v =>
+            {
+                p2ContentEvents.Add(v);
+            });
+
+
+            eventBus.Publish(p1Login);
+            eventBus.Publish(p2Login);
+            eventBus.Publish(p3Login);
+
+            var plus1 = new PlusCode("8FX9XW2F+9X", 10);
+            var plus2 = new PlusCode("8FX9XW2F+8X", 10);
+            var plus3 = new PlusCode("8FX9XW2F+7X", 10);
+            var plus4 = new PlusCode("8FX9XW2F+6X", 10);
+            var plus5 = new PlusCode("8FX9XW2F+5X", 10);
+
+
+
+            //Spawn player on tile
+            p1Gps.OnNext(plus1);
+            //player has not moved yet, only one tile should be affected
+            Assert.IsTrue(mapEvents.Count == 1);
+
+
+
+            //Spawn p2
+            p2Gps.OnNext(plus2);
+
+
+
+
+            //Both players see each other check:
+
+            var p2Content = from e in p2ContentEvents
+                            from e2 in e.VisibleContent
+                            from e3 in e2.Value
+                            where e3 is Player
+                            select e3;
+
+            var p2VisiblePlayers = p2Content.Count();
+
+
+            var p1Content = from e in p1ContentEvents
+                            from e2 in e.VisibleContent
+                            from e3 in e2.Value
+                            where e3 is Player
+                            select e3;
+            var p1VisiblePlayers = p1Content.Count();
+
+
+            Assert.IsTrue(p1ContentEvents.Count == 2);
+            Assert.IsTrue(p2ContentEvents.Count == 2);
+            Assert.IsTrue(p2VisiblePlayers == 2);
+            Assert.IsTrue(p1VisiblePlayers == 2);
+
+            p1ContentEvents.Clear();
+            p2ContentEvents.Clear();
+
+            //Player 2 moves on same tile as player 1
+            
+            p2Gps.OnNext(plus1);
+
+            p2Content = from e in p2ContentEvents
+                        from e2 in e.VisibleContent
+                        from e3 in e2.Value
+                        where e3 is Player
+                        select e3;
+
+            p2VisiblePlayers = p2Content.Count();
+
+
+            p1Content = from e in p1ContentEvents
+                        from e2 in e.VisibleContent
+                        from e3 in e2.Value
+                        where e3 is Player
+                        select e3;
+            p1VisiblePlayers = p1Content.Count();
+
+            //Only 1 player moved:
+            Assert.IsTrue(p1ContentEvents.Count == 1);
+            Assert.IsTrue(p2ContentEvents.Count == 1);
+            //Two players should be visible
+            Assert.IsTrue(p2VisiblePlayers == 2);
+            Assert.IsTrue(p1VisiblePlayers == 2);
+        }
 
 
     }
