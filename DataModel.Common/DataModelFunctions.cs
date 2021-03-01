986ceacb67e44473d6ce86f15c05099c14fb61ae
Dk <dvsk76@outlook.com>
2021-03-01 19:31:27 +0100

    Changed object serialization

--- DataModel.Common/DataModelFunctions.cs
+++ DataModel.Common/DataModelFunctions.cs
@@ -19,20 +19,11 @@ namespace DataModel.Common
         //TODO: Temporary cast  imessage to concrete message
         public static byte[] ToJsonPayload(this IMessage msg)
         {
-            switch (msg)
+            var serialized = JsonConvert.SerializeObject(msg, new JsonSerializerSettings()
             {
-                case BatchContentMessage concreteType:
-                    var serialized = JsonConvert.SerializeObject(concreteType, new JsonSerializerSettings()
-                    {
-                        TypeNameHandling = TypeNameHandling.Objects
-                    });
-                    return Encoding.UTF8.GetBytes(serialized);
-                default:
-                    return Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(msg, new JsonSerializerSettings()
-                    {
-                        TypeNameHandling = TypeNameHandling.All
-                    }));
-            }
+                TypeNameHandling = TypeNameHandling.Objects
+            });
+            return Encoding.UTF8.GetBytes(serialized);
         }
         public static IMessage FromJsonPayload(this byte[] payload)
         {
@@ -45,7 +36,7 @@ namespace DataModel.Common
                 TypeNameHandling = TypeNameHandling.Auto
             });
         }
-        
+
         public static PlusCode From10String(this string location)
         {
             return new PlusCode(location, 10);
@@ -71,7 +62,7 @@ namespace DataModel.Common
         public static GPS AddOFfset(this GPS gps, double latOffset, double lonOffset)
             => new GPS(gps.Lat + latOffset, gps.Lon + lonOffset);
 
-        
+
 
         public static List<GPS> GPSNodesWithOffsets(GPS startPos, double latNodeOffset, double lonNodeOffset, int nodeAmount)
         {
@@ -87,7 +78,7 @@ namespace DataModel.Common
             double angleStep = 360d / points;
             Math.Floor(angleStep);
             var nodes = new List<GPS>();
-            for (int i = 1; i < points+1; i++)
+            for (int i = 1; i < points + 1; i++)
             {
                 double lat = startPos.Lat + (radius * Math.Cos(angleStep * i));
                 double lon = startPos.Lon + (radius * Math.Sin(angleStep * i));
@@ -97,8 +88,8 @@ namespace DataModel.Common
         }
 
         public static PlusCode ToLowerResolution(this PlusCode code, int target)
-            => new PlusCode(string.Concat(code.Code.Take(target+1)), target);
+            => new PlusCode(string.Concat(code.Code.Take(target + 1)), target);
 
-        
+
     }
 }
